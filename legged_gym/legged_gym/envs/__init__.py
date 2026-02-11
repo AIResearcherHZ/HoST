ce0bfea9194abf209de23bc3eb6091d147d6dd1d
AIResearcherHZ <xhz2082416211@gmail.com>
2026-02-11 12:02:00 +0800

    [New Robot] 添加Taks_T1机器人支持，包含ground、platform、wall、slope和ground_prone五种环境配置，并在URDF中新增关键帧链接

--- legged_gym/legged_gym/envs/__init__.py
+++ legged_gym/legged_gym/envs/__init__.py
@@ -28,6 +28,17 @@ from legged_gym.envs.pi.pi_config_ground import PiCfg as PiCfgGround
 from legged_gym.envs.pi.pi_config_ground import PiCfgPPO as PiCfgPPOGround
 from legged_gym.envs.pi.pi_host_ground import LeggedRobot_Pi
 
+from legged_gym.envs.taks_t1.taks_t1_config_ground import TaksT1Cfg as TaksT1CfgGround
+from legged_gym.envs.taks_t1.taks_t1_config_ground import TaksT1CfgPPO as TaksT1CfgPPOGround
+from legged_gym.envs.taks_t1.taks_t1_config_platform import TaksT1Cfg as TaksT1CfgPlatform
+from legged_gym.envs.taks_t1.taks_t1_config_platform import TaksT1CfgPPO as TaksT1CfgPPOPlatform
+from legged_gym.envs.taks_t1.taks_t1_config_slope import TaksT1Cfg as TaksT1CfgSlope
+from legged_gym.envs.taks_t1.taks_t1_config_slope import TaksT1CfgPPO as TaksT1CfgPPOSlope
+from legged_gym.envs.taks_t1.taks_t1_config_wall import TaksT1Cfg as TaksT1CfgWall
+from legged_gym.envs.taks_t1.taks_t1_config_wall import TaksT1CfgPPO as TaksT1CfgPPOWall
+from legged_gym.envs.taks_t1.taks_t1_config_ground_prone import TaksT1Cfg as TaksT1CfgGroundProne
+from legged_gym.envs.taks_t1.taks_t1_config_ground_prone import TaksT1CfgPPO as TaksT1CfgPPOGroundProne
+
 task_registry.register( "g1_ground", LeggedRobotGround, G1CfgGround(), G1CfgPPOGround())
 task_registry.register( "g1_platform", LeggedRobotPlatform, G1CfgPlatform(), G1CfgPPOPlatform())
 task_registry.register( "g1_wall", LeggedRobotWall, G1WallCfgWall(), G1WallCfgPPOWall())
@@ -36,3 +47,9 @@ task_registry.register( "g1_ground_prone", LeggedRobotGroundProne, G1CfgGroundPr
 task_registry.register( "h1_ground", LeggedRobotGround, H1CfgGround(), H1CfgPPOGround())
 
 task_registry.register( "pi_ground", LeggedRobot_Pi, PiCfgGround(), PiCfgPPOGround())
+
+task_registry.register( "taks_t1_ground", LeggedRobotGround, TaksT1CfgGround(), TaksT1CfgPPOGround())
+task_registry.register( "taks_t1_platform", LeggedRobotPlatform, TaksT1CfgPlatform(), TaksT1CfgPPOPlatform())
+task_registry.register( "taks_t1_wall", LeggedRobotWall, TaksT1CfgWall(), TaksT1CfgPPOWall())
+task_registry.register( "taks_t1_slope", LeggedRobotSlope, TaksT1CfgSlope(), TaksT1CfgPPOSlope())
+task_registry.register( "taks_t1_ground_prone", LeggedRobotGroundProne, TaksT1CfgGroundProne(), TaksT1CfgPPOGroundProne())
